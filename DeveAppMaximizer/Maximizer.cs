a9b355ec7f6730097280fa44e68f90ff1c9a0acd
Devedse <davydavidse@gmail.com>
2020-02-16 13:23:41 +0100

    Maximizer added

---
+++ DeveAppMaximizer/Maximizer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace DeveAppMaximizer
+{
+    public static class Maximizer
+    {
+        private const int SW_MAXIMIZE = 3;
+        private const int SW_MINIMIZE = 6;
+        // more here: http://www.pinvoke.net/default.aspx/user32.showwindow
+
+        [DllImport("user32.dll", EntryPoint = "FindWindow")]
+        private static extern IntPtr FindWindowByCaption(IntPtr ZeroOnly, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        public static void Show(IntPtr hWnd)
+        {
+            ShowWindow(hWnd, SW_MINIMIZE);
+        }
+    }
+}
--- DeveAppMaximizer/Program.cs
+++ DeveAppMaximizer/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
 
 namespace DeveAppMaximizer
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Error: No command specified");
+            }
+            else
+            {
+                var appToStart = args[0];
+
+                var arguments = "";
+
+                if (args.Length > 1)
+                {
+                    arguments = string.Join(' ', args.Skip(1));
+                }
+
+                Console.WriteLine($"Starting '{appToStart}' with arguments '{arguments}'...");
+
+                var psi = new ProcessStartInfo(appToStart, arguments);
+
+                var process = Process.Start(psi);
+
+                Thread.Sleep(3000);
+                Console.WriteLine($"Maximizing now...");
+                Maximizer.Show(process.MainWindowHandle);
+            }
         }
     }
 }
